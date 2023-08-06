099174d6e7a4e939dcb172b9d2d6ecdd478c6bba
Ryan Andersen <ryco117@gmail.com>
2023-08-05 20:43:15 -0700

    Continued minor cleanup

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 nix:
 	nasm -f elf64 -o AES.o AES.asm
-	g++ -c main.cpp -o main.o
-	g++ -o a.out AES.o main.o
+	g++ -c main.cpp -o main.o -O3
+	g++ -o a.out AES.o main.o -O3
 win:
 	nasm -f win64 -o AES.o AES.asm
-	g++ -c main.cpp -o main.o -DWINDOWS
-	g++ -o a.exe AES.o main.o
+	g++ -c main.cpp -o main.o -O3 -DWINDOWS
+	g++ -o a.exe AES.o main.o -O3
--- main.cpp
+++ main.cpp
@@ -5,56 +5,61 @@
 extern "C"
 {
 	bool AESNI();
-	void EncryptNix(const uint8_t* Text, int size, const uint8_t IV[16], const uint8_t Key[32], uint8_t* Buffer, bool usePKCS7Padding);
-	int DecryptNix(const uint8_t* Cipher, int size, const uint8_t IV[16], const uint8_t Key[32], uint8_t* Buffer, bool expectPKCS7Padding);
-	void EncryptWin(const uint8_t* Text, int size, const uint8_t IV[16], const uint8_t Key[32], uint8_t* Buffer, bool usePKCS7Padding);
-	int DecryptWin(const uint8_t* Cipher, int size, const uint8_t IV[16], const uint8_t Key[32], uint8_t* Buffer, bool expectPKCS7Padding);
+	void EncryptNix(const uint8_t* data, int size, const uint8_t IV[16], const uint8_t key[32], uint8_t* buffer, bool usePKCS7Padding);
+	int64_t DecryptNix(const uint8_t* Cipher, int size, const uint8_t IV[16], const uint8_t key[32], uint8_t* buffer, bool expectPKCS7Padding);
+	void EncryptWin(const uint8_t* data, int size, const uint8_t IV[16], const uint8_t key[32], uint8_t* buffer, bool usePKCS7Padding);
+	int64_t DecryptWin(const uint8_t* Cipher, int size, const uint8_t IV[16], const uint8_t key[32], uint8_t* buffer, bool expectPKCS7Padding);
 }
 
 int main()
 {
+	// Don't use non-random, hardcoded keys and IVs in production code. This is just for demonstration.
 	uint8_t IV[16] = {0xFF, 0xFE, 0xFD, 0xFC, 0xFB, 0xFA, 0xF9, 0xF8, 0xF7, 0xF6, 0xF5, 0xF4, 0xF3, 0xF2, 0xF1, 0xF0};
-	uint8_t Key[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};
-	std::string Message = "Hello there world! This tests arbitrarily sized plaintext! Did it work?!?";
+	uint8_t key[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};
+	std::string message = "Hello there world! This tests arbitrarily sized plaintext! Did it work?!?";
 	char buff[81] = {"\0"};
+
+	// Recommended to use PKCS7 padding when the size of the source data is not known at decryption time.
+	// This is common for user generated data, such as text.
 	bool usePKCS7Padding = true;
 	
 	if(!AESNI())
 	{
-		std::cout << "AES-NI is not supported on this CPU :(\n";
+		std::cout << "AES-NI is not supported on this CPU :(" << std::endl;
 		return -1;
 	}
 	
-	bool p = true;
-	if(p)
+	bool print = true;
+	if(print)
 	{
-		std::cout << "Original: " << Message << std::endl;
-		/*std::cout << "Message Ptr: " << (unsigned long long)Message.c_str() << std::endl << "IV Ptr: " << (unsigned long long)IV << std::endl\
-				  << "Key Ptr: " << (unsigned long long)Key << std::endl << "buff Ptr: " << (unsigned long long)buff << std::endl;*/
+		std::cout << "Original: " << message << std::endl;
 	}
 	#ifdef WINDOWS
 		EncryptWin((uint8_t*)Message.c_str(), Message.size(), IV, Key, (uint8_t*)buff, usePKCS7Padding);
 	#else
-		EncryptNix((uint8_t*)Message.c_str(), Message.size(), IV, Key, (uint8_t*)buff, usePKCS7Padding);
+		EncryptNix((uint8_t*)message.c_str(), message.size(), IV, key, (uint8_t*)buff, usePKCS7Padding);
 	#endif
-	if(p)
+	if(print)
+	{
 		std::cout << "Encrypted: " << buff << std::endl;
+	}
 
 	#ifdef WINDOWS
 		int len = DecryptWin((const uint8_t*)buff, 80, IV, Key, (uint8_t*)buff, usePKCS7Padding);
 	#else
-		int len = DecryptNix((const uint8_t*)buff, 80, IV, Key, (uint8_t*)buff, usePKCS7Padding);
+		int len = DecryptNix((const uint8_t*)buff, 80, IV, key, (uint8_t*)buff, usePKCS7Padding);
 	#endif
 	if(len == -1)
 	{
-		if(p)
+		if(print)
 			std::cerr << "Was not padded correctly\n";
 		return len;
 	}
-	if(p)
+	if(print)
 	{
 		std::cout << "Decrypted: " << buff << std::endl;
 		std::cout << "String Length: " << strlen(buff) << ", " << len << std::endl;
 	}
+
 	return 0;
 }
